52ff45d075e770978467e69129f47dc0be24448d
Alec Malcolm <wmalcolm94@outlook.com>
2022-01-21 17:50:02 -0500

    Finishes ticketing service module

---
+++ client/api/build-client.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const buildClient = ({ req }) => {
+  if (typeof window === 'undefined') {
+    return axios.create({ 
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers
+    });
+  } else {
+    return axios.create({ baseUrl: '/' });
+  }
+};
+
+export default buildClient;
