2399b5131797a2b73159402620de2a65264ab42f
Alec Malcolm <wmalcolm94@outlook.com>
2022-05-20 13:36:44 -0400

    Starting payment service

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@wamtickets/common';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
-import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
-import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
-
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
@@ -30,14 +26,9 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
-    new TicketCreatedListener(natsWrapper.client).listen();
-    new TicketUpdatedListener(natsWrapper.client).listen();
-    new ExpirationCompleteListener(natsWrapper.client).listen();
-
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+      autoCreate: true,
+      autoIndex: true
     });
     console.log('Connected to MongoDb');
   } catch (err) {
